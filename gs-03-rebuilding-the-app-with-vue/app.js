60b52a02a68f8e9fe747e040e4838703a273f68d
Danny <danny.vaneeckhoutte@gmail.com>
2021-01-22 13:22:33 +0100

    Rebuild the app w/ Vua

--- gs-03-rebuilding-the-app-with-vue/app.js
+++ gs-03-rebuilding-the-app-with-vue/app.js
@@ -1,18 +1,3 @@
-Vue.createApp({
-  data() {
-    return {
-      goals: [],
-      enteredValue: ''
-    };
-  },
-  methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = '';
-    }
-  }
-}).mount('#app');
-
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEl = document.querySelector('ul');
@@ -26,3 +11,18 @@ Vue.createApp({
 // }
 
 // buttonEl.addEventListener('click', addGoal);
+
+const app = Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: "",
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = "";
+        },
+    },
+}).mount("#app");
