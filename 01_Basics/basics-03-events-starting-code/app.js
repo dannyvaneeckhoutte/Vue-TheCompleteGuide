f793654cea31eb8f0ed3951e9881775599097e61
dannyvaneeckhoutte <60157535+dannyvaneeckhoutte@users.noreply.github.com>
2021-02-08 13:25:11 +0100

    Events

--- 01_Basics/basics-03-events-starting-code/app.js
+++ 01_Basics/basics-03-events-starting-code/app.js
@@ -2,10 +2,13 @@ const app = Vue.createApp({
 	data() {
 		return {
 			counter: 0,
-			name: "Danny",
+			name: "",
 		};
 	},
 	methods: {
+		setName(event, lastName) {
+			this.name = event.target.value + " " + lastName;
+		},
 		add(num) {
 			return (this.counter = this.counter + num);
 		},
