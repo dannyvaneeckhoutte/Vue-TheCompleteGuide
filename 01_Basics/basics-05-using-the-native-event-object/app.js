278e4af968805b16f03afcd9aaa2c7c187e90cb0
dannyvaneeckhoutte <60157535+dannyvaneeckhoutte@users.noreply.github.com>
2021-02-10 09:47:22 +0100

    Methods vs computed vs watch

--- 01_Basics/basics-05-using-the-native-event-object/app.js
+++ 01_Basics/basics-05-using-the-native-event-object/app.js
@@ -3,13 +3,56 @@ const app = Vue.createApp({
 		return {
 			counter: 0,
 			name: "",
+			lastName: "",
+			// fullName: "",
 		};
 	},
+	watch: {
+		counter(value) {
+			if (value > 50) {
+				this.counter = 0;
+			}
+		},
+		/* 		name(value) {
+			if (value === "") {
+				this.fullName = "";
+			} else {
+				this.fullName = value + " " + this.lastName;
+			}
+		},
+		lastName(value) {
+			if (value === "") {
+				this.fullName = "";
+			} else {
+				this.fullName = this.name + " " + value;
+			}
+		}, */
+	},
+	computed: {
+		fullName() {
+			if (this.name === "" || this.lastName === "") {
+				return "";
+			} else {
+				return this.name + " " + this.lastName;
+			}
+		},
+	},
+
 	methods: {
+		/* 		// replaced by computed property fullName()
+		outputFullname() {
+			// If the input field is empty, return empty string, else return the full name
+			if (this.name === "") {
+				return "";
+			} else {
+				return this.name + " " + "Schwarzmüller";
+			}
+		}, */
+
 		// Not needed, using v-model (2-way binding) instead to update name:
-		// setName(event) {
-		// 	this.name = event.target.value;
-		// },
+		setName(event) {
+			this.name = event.target.value;
+		},
 		resetInput() {
 			this.name = "";
 		},
@@ -20,6 +63,9 @@ const app = Vue.createApp({
 		reduce(num) {
 			this.counter = this.counter - num;
 			// this.counter--;
+		},
+		resetResult() {
+			this.counter = 0;
 		},
 	},
 });
--- Notes.md
+++ Notes.md
@@ -39,3 +39,22 @@ const app = Vue.createApp({
 });
 app.mount("#user-goal");
 ```
+
+### App properties
+
+#### data
+
+The function that returns a data object for the component instance.
+
+#### methods
+
+Only use methods if you know you want to recalculate a value whenever anything changes, else use computed.
+Methods to be mixed into the component instance. You can access these methods directly on the VM instance, or use them in directive expressions. All methods will have their this context automatically bound to the component instance.
+
+#### computed
+
+Computed properties are cached, and only re-computed on reactive dependency changes. Note that if a certain dependency is out of the instance's scope (i.e. not reactive), the computed property will not be updated.
+
+#### watch
+
+Are
